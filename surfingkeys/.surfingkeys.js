84f6412fdc1b24307bc887f49f57d90ac2b1bf17
Michał Jabczyk <13063089+jabczyk@users.noreply.github.com>
2024-07-09 10:43:14 +0200

    feat: add surfingkeys config

---
+++ surfingkeys/.surfingkeys.js
@@ -0,0 +1,19 @@
+api.unmap("t");
+api.unmap("T");
+
+api.Hints.style(
+  `font-size: 11px;
+  font-family: sans-serif;
+  letter-spacing: 1px;`
+);
+
+api.mapkey(";g", "Genius", () => {
+  if (window.location.hostname !== "www.youtube.com") {
+    return;
+  }
+
+  const title = document.title.replace("- YouTube", "");
+  const query = `site:genius.com ${title}`;
+  const url = `https://www.google.com/search?q=${query}`;
+  window.open(url, "_blank").focus();
+});
